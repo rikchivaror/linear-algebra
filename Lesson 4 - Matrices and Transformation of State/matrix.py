9ae6f9113b61b9a0389e499f5bb74307f59b8d8b
rikchivaror <2Et2tzXw2EYocAAQBGmQ>
2019-01-24 17:27:29 -0700

    fix: Improve __str__ method to print matrices more aesthetically

--- Lesson 4 - Matrices and Transformation of State/matrix.py
+++ Lesson 4 - Matrices and Transformation of State/matrix.py
@@ -1,56 +1,64 @@
-from vector import Vector
-from decimal import Decimal, getcontext
-
-getcontext().prec = 30
+# from vector import Vector
 
 
 class Matrix(object):
 
     ALL_VECTORS_MUST_BE_IN_SAME_DIM_MSG = 'All rows should have the same dimension'
 
-    def __init__(self, vectors):
+    def __init__(self, matrix):
 
         try:
-            d = vectors[0].dimension
-            for v in vectors:
-                assert v.dimension == d
+            d = len(matrix[0])
+            for v in matrix:
+                assert len(v) == d
 
-            self.vectors = vectors
+            self.matrix = matrix
             self.row_dimension = d
-            self.col_dimension = len(vectors)
+            self.col_dimension = len(matrix)
 
         except AssertionError:
             raise Exception(self.ALL_VECTORS_MUST_BE_IN_SAME_DIM_MSG)
 
     def scalar_mult(self, c):
-        pass
+        scaled = []
+
+        for i in range(self.col_dimension):
+            scaled.append([])
+            for j in range(self.row_dimension):
+                scaled[i].append(c * self.matrix[i][j])
+
+        return Matrix(scaled)
 
     def __str__(self):
         precision = 2
         ret = 'Matrix:\n'
-        max_length = 0
+        max_col_length = [0] * self.row_dimension
 
         for i in range(self.col_dimension):
-            for e in self.vectors[i]:
-                length = len(str(int(e)))
-                if length > max_length:
-                    max_length = length
+            for j, e in enumerate(self.matrix[i]):
+                int_length = str(float(e)).index('.')
+                col_length = int_length + precision + 3     # constant '3' takes into account two empty spaces to the
+                if col_length > max_col_length[j]:          # left of the number and the space occupied by the decimal
+                    max_col_length[j] = col_length
 
         for i in range(self.col_dimension):
-            for e in self.vectors[i]:
-                ret += '{0:{width}.{prec}f}'.format(e, width=max_length+precision+3, prec=precision)
+            for j, e in enumerate(self.matrix[i]):
+                ret += '{0:{width}.{prec}f}'.format(e, width=max_col_length[j], prec=precision)
             ret += '\n'
 
         return ret
 
 
 def test():
-    v1 = Vector([1.5236, -.354, 3])
-    v2 = Vector([4, 5, 6])
+    v1 = [-0.56, -1234.354, 3]
+    v2 = [1.5236, 5, 6]
 
     M = Matrix([v1, v2])
 
     print(M)
+
+    Z = M.scalar_mult(3)
+    print(Z)
 
 
 if __name__ == '__main__':
